f9bbad7c6d30b0d692dea6477a9ca3943f9a63f5
Muhammad Alfian Ma'ruf <alfianm264@gmail.com>
2020-09-30 10:07:34 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ponny Beaute app',
+      title: 'Ponny Beaute',
       theme: ThemeData(
         primaryColor: Color(0xFFFDF8F0),
       ),
