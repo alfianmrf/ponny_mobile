fc9f04292b52052078a989bd8e7f35543f6a5e76
Muhammad Alfian Ma'ruf <alfianm264@gmail.com>
2020-10-02 08:51:29 +0700

    home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ponny/screens/login.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
@@ -81,17 +82,194 @@ class _HomeScreenState extends State<HomeScreen> {
             extendBodyBehindAppBar: true,
             appBar: new PonnyAppBar(),
             body: new Container(
-              child: new Swiper(
-                itemBuilder: (BuildContext context, int index) {
-                  return new Image.network(
-                    "http://via.placeholder.com/288x188",
-                    fit: BoxFit.fill,
-                  );
-                },
-                itemCount: 3,
-                pagination: new SwiperPagination(),
-                control: new SwiperControl(),
-              ),
+              color: Theme.of(context).primaryColor,
+              child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 300,
+                        child: new Swiper(
+                          itemBuilder: (BuildContext context, int index) {
+                            return new Image.network(
+                              "http://via.placeholder.com/288x188",
+                              fit: BoxFit.cover,
+                            );
+                          },
+                          itemCount: 3,
+                          pagination: new SwiperPagination(
+                              margin: new EdgeInsets.all(5.0),
+                              builder: new DotSwiperPaginationBuilder(
+                                  color: Color(0xffE6E7E9), activeColor: Color(0xffF48262))
+                          ),
+                          control: null,
+                          autoplay: true,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Text(
+                          'Best Seller',
+                          style: TextStyle(
+                            fontFamily: 'Yeseva',
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 135,
+                        color: Colors.blue,
+                        child: new Swiper(
+                          itemBuilder: (BuildContext context, int index) {
+                            return GridView.count(
+                              primary: false,
+                              padding: const EdgeInsets.symmetric(horizontal: 10),
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                              childAspectRatio: 0.8,
+                              crossAxisCount: 3,
+                              children: <Widget>[
+                                Stack(
+                                  children: <Widget>[
+                                    Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      child: Image.asset(
+                                        "assets/images/produk.png",
+                                        fit: BoxFit.contain,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(top: 10.0),
+                                      child: Align(
+                                          alignment: Alignment.topLeft,
+                                          child: Container(
+                                            child: Padding(
+                                              padding: EdgeInsets.symmetric(horizontal: 5.0),
+                                              child: const Text(
+                                                '35%',
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontFamily: 'Brandon'
+                                                ),
+                                              ),
+                                            ),
+                                            color: Color(0xffF48262),
+                                          )
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(5.0),
+                                      child: Align(
+                                        alignment: Alignment.topRight,
+                                        child: Icon(
+                                          Icons.favorite_border,
+                                          color: Color(0xffF48262),
+                                          size: 20,
+                                        ),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.bottomCenter,
+                                      child: Container(
+                                        width: MediaQuery.of(context).size.width,
+                                        child: const Text(
+                                          'ADD TO BAG',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontFamily: 'Brandon'
+                                          ),
+                                        ),
+                                        color: Color(0xffF3C1B5),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Stack(
+                                  children: <Widget>[
+                                    Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      child: Image.asset(
+                                        "assets/images/produk.png",
+                                        fit: BoxFit.contain,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(5.0),
+                                      child: Align(
+                                        alignment: Alignment.topRight,
+                                        child: Icon(
+                                          Icons.favorite_border,
+                                          color: Color(0xffF48262),
+                                          size: 20,
+                                        ),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.bottomCenter,
+                                      child: Container(
+                                        width: MediaQuery.of(context).size.width,
+                                        child: const Text(
+                                          'ADD TO BAG',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontFamily: 'Brandon'
+                                          ),
+                                        ),
+                                        color: Color(0xffF3C1B5),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Stack(
+                                  children: <Widget>[
+                                    Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      child: Image.asset(
+                                        "assets/images/produk.png",
+                                        fit: BoxFit.contain,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(5.0),
+                                      child: Align(
+                                        alignment: Alignment.topRight,
+                                        child: Icon(
+                                          Icons.favorite_border,
+                                          color: Color(0xffF48262),
+                                          size: 20,
+                                        ),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.bottomCenter,
+                                      child: Container(
+                                        width: MediaQuery.of(context).size.width,
+                                        child: const Text(
+                                          'ADD TO BAG',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontFamily: 'Brandon'
+                                          ),
+                                        ),
+                                        color: Color(0xffF3C1B5),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            );
+                          },
+                          itemCount: 3,
+                          pagination: null,
+                          control: null,
+                          autoplay: false,
+                        ),
+                      ),
+                    ],
+                  )
+              )
             ),
           ),
         ]
