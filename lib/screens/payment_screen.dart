f2f7e9314323778fb7f119f36b26646af858dc29
Muhammad Alfian Ma'ruf <alfianm264@gmail.com>
2020-10-12 11:00:29 +0700

    payment

--- lib/screens/payment_screen.dart
+++ lib/screens/payment_screen.dart
@@ -88,18 +88,355 @@ class _PaymentScreenState extends State<PaymentScreen> {
                               ),
                               Container(
                                 width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/images/payment/bca-02.png',
+                                          height: 40,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 10),
+                                          child: Text(
+                                            'BCA Virtual Account',
+                                            style: TextStyle(
+                                              fontFamily: 'Brandon',
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.chevron_right,
+                                      color: Color(0xffF48262),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/images/payment/bri-02.png',
+                                          height: 40,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 10),
+                                          child: Text(
+                                            'BRI BRIVA',
+                                            style: TextStyle(
+                                              fontFamily: 'Brandon',
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.chevron_right,
+                                      color: Color(0xffF48262),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                color: Color(0xffFDEDE4),
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                                child: Text(
+                                  'BANK TRANSFER (Konfirmasi Manual)',
+                                  style: TextStyle(
+                                    fontFamily: 'Brandon',
+                                    fontSize: 14,
+                                    color: Color(0xffF48262),
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/images/payment/bca-02.png',
+                                          height: 40,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 10),
+                                          child: Text(
+                                            'Bank BCA',
+                                            style: TextStyle(
+                                              fontFamily: 'Brandon',
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.chevron_right,
+                                      color: Color(0xffF48262),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/images/payment/mandiri-02.png',
+                                          height: 40,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 10),
+                                          child: Text(
+                                            'Bank Mandiri',
+                                            style: TextStyle(
+                                              fontFamily: 'Brandon',
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.chevron_right,
+                                      color: Color(0xffF48262),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/images/payment/permata-02.png',
+                                          height: 40,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 10),
+                                          child: Text(
+                                            'Bank Permata',
+                                            style: TextStyle(
+                                              fontFamily: 'Brandon',
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.chevron_right,
+                                      color: Color(0xffF48262),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
                                 padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                                 child: Row(
                                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Wrap(
+                                    Row(
                                       children: [
-                                        Image.asset('assets/images/payment/bca-02.png'),
-                                        Text(
-                                          'BCA VIRTUAL ACCOUNT',
-                                          style: TextStyle(
-                                            fontFamily: 'Brandon',
-                                            fontSize: 14,
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 5),
+                                          child: Text(
+                                            'Bank Lain',
+                                            style: TextStyle(
+                                              fontFamily: 'Brandon',
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.chevron_right,
+                                      color: Color(0xffF48262),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                color: Color(0xffFDEDE4),
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                                child: Text(
+                                  'MOBILE PAYMENT',
+                                  style: TextStyle(
+                                    fontFamily: 'Brandon',
+                                    fontSize: 14,
+                                    color: Color(0xffF48262),
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/images/payment/ovo-02.png',
+                                          height: 40,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 10),
+                                          child: Text(
+                                            'OVO',
+                                            style: TextStyle(
+                                              fontFamily: 'Brandon',
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.chevron_right,
+                                      color: Color(0xffF48262),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/images/payment/gopay-02.png',
+                                          height: 40,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 10),
+                                          child: Text(
+                                            'GOPAY',
+                                            style: TextStyle(
+                                              fontFamily: 'Brandon',
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.chevron_right,
+                                      color: Color(0xffF48262),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                color: Color(0xffFDEDE4),
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                                child: Text(
+                                  'KARTU KREDIT/DEBIT',
+                                  style: TextStyle(
+                                    fontFamily: 'Brandon',
+                                    fontSize: 14,
+                                    color: Color(0xffF48262),
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/images/payment/visa-02.png',
+                                          height: 20,
+                                        ),
+                                        Image.asset(
+                                          'assets/images/payment/mastercard-02.png',
+                                          height: 20,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 10),
+                                          child: Text(
+                                            'Credit Card / Debit Card',
+                                            style: TextStyle(
+                                              fontFamily: 'Brandon',
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.chevron_right,
+                                      color: Color(0xffF48262),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                color: Color(0xffFDEDE4),
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                                child: Text(
+                                  'METODE PEMBAYARAN LAINNYA',
+                                  style: TextStyle(
+                                    fontFamily: 'Brandon',
+                                    fontSize: 14,
+                                    color: Color(0xffF48262),
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 5),
+                                          child: Text(
+                                            'Credit Card / Debit Card',
+                                            style: TextStyle(
+                                              fontFamily: 'Brandon',
+                                              fontSize: 14,
+                                            ),
                                           ),
                                         ),
                                       ],
