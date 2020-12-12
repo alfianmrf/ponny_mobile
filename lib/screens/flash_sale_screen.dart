02353d35af8c211c5bc45d41f471d7dec456389f
joko styawan <jokostyawan267@gmail.com>
2020-12-12 09:39:20 +0700

    up

--- lib/screens/flash_sale_screen.dart
+++ lib/screens/flash_sale_screen.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_countdown_timer/current_remaining_time.dart';
 import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:hexcolor/hexcolor.dart';
